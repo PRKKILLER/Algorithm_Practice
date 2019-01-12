6536aea122874f77b4f4b4f858371fe08b3c9923
WonderfulXue <dw6000@163.com>
2019-01-12 10:03:46 +0800

    update 0066 solution

---
+++ 0066.Plus One/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    '''
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list,
+and each element in the array contain a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+Example: input = [1,2,3] -> output = [1,2,4]
+
+    '''
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        carry = 1
+        n = len(digits) - 1
+        while n >= 0:
+            num = (digits[n] + carry) % 10
+            carry = (digits[n] + carry) // 10
+            digits[n] = num
+            if carry == 0:
+                return digits
+            n -= 1
+
+        return [1] + [0] * len(digits) # 如果到了最高位进位仍然存在，那么我们必须重新new一个数组，然后把第一个为赋成1
+
+print(Solution().plusOne([9,9,9]))
